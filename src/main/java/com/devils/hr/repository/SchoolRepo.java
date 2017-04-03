567602a9a6e021770c8d25101ba5466bee0eede2
andy <andy.luming@outlook.com>
2017-04-03 14:11:21 +0800

    add annotation

--- src/main/java/com/devils/hr/repository/SchoolRepo.java
+++ src/main/java/com/devils/hr/repository/SchoolRepo.java
@@ -2,10 +2,12 @@ package com.devils.hr.repository;
 
 import com.devils.hr.pojo.roles.School;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by AndyL on 2017/4/2.
  */
+@Repository
 public interface SchoolRepo extends MongoRepository<School, String> {
 
 }
--- src/main/java/com/devils/hr/repository/StudentRepo.java
+++ src/main/java/com/devils/hr/repository/StudentRepo.java
@@ -2,10 +2,12 @@ package com.devils.hr.repository;
 
 import com.devils.hr.pojo.roles.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by AndyL on 2017/4/2.
  */
+@Repository
 public interface StudentRepo extends MongoRepository<Student, String>{
 
 }
